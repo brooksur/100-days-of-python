32616492db45182ee07cc0ded60bfa5b935deeba
Brooks Benson <brooks@pdrutah.com>
2024-01-30 11:51:35 -0700

    added comments

--- day-1-printing-start/main.py
+++ day-1-printing-start/main.py
@@ -1,9 +1,15 @@
+# Get the users name
 name = input("What is your name?\n")
 
+# Greet the user
 print('Hello, ' + name + '! Welcome to Band Name Generator!')
 
+# Get the users city and pet name
 city = input('What\'s the name of the city you grew up in?\n')
 pet = input('What\'s your pet\'s name?\n')
+
+# Generate the band name
 band_name = city + ' ' + pet
 
+# Print the band name
 print('Your band name could be ' + band_name)
